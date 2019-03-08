9d4e0fa2d0f6c64ed96571554e2c306631745d7c
Lille-R-Ming <tlddliujinming@163.com>
2019-03-08 10:15:02 +0800

    创建Utils

---
+++ src/main/java/red/lille/utils/Heap.java
@@ -0,0 +1,12 @@
+package red.lille.utils;
+
+import java.util.Collection;
+
+/**
+ * 
+ * @author lille
+ *
+ * @param <T> 这个堆要存放的对象
+ */
+public interface Heap<T extends Comparable<T>> extends Collection<T>{
+}
